1f9c2967d8def84a19ef32a66afd0449462df339
bikke777 <canacana2@gmail.com>
2022-07-05 17:25:52 +0900

    0705

--- models/StatusRepository.php
+++ models/StatusRepository.php
@@ -7,7 +7,7 @@ class StatusRepository extends DbRepository
    */
   public function insert($user_id, $body)
   {
-    $now = new DataTime();
+    $now = new DateTime();
     $sql = "
       INSERT INTO status(user_id, body, created_at)
         VALUES(:user_id, :body, :created_at)
@@ -32,5 +32,38 @@ class StatusRepository extends DbRepository
         ORDER BY a.created_at DESC
       ";
     return $this->fetchAll($sql, array(':user_id' => $user_id));
+  }
+
+  /**
+   * ユーザーIDに一致する投稿を、投稿日の降順に全件取得する
+   */
+  public function fetchAllByUserId($user_id)
+  {
+    $sql = "
+      SELECT a.*, u.user_name
+        FROM status a
+          LEFT JOIN user u ON a.user_id = u.id
+        WHERE u.id = :user_id
+        ORDER BY a.created_at DESC
+    ";
+    return $this->fetchAll($sql, array(':user_id' => $user_id));
+  }
+
+  /**
+   * 投稿IDとユーザーのIDに一致するレコードを1件取得する
+   */
+  public function fetchByIdAndUserName($id, $user_name)
+  {
+    $sql = "
+      SELECT a.*, u.user_name
+        FROM status a
+          LEFT JOIN user u ON u.id = a.user_id
+        WHERE a.id = :id
+          AND u.user_name = :user_name
+    ";
+    return $this->fetch($sql, array(
+      ':id' => $id,
+      ':user_name' => $user_name,
+    ));
   }
 }
